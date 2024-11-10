6b6f847a65dc06078afaee98d5bf043470777840
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-11-10 22:08:16 +0800

    fix/bloom filter and cache usage

--- src/main/java/edu/smu/smusql/Engine.java
+++ src/main/java/edu/smu/smusql/Engine.java
@@ -22,7 +22,7 @@ public class Engine {
      * @param size
      * @param hashCount
      */
-    private static final int FILTER_SIZE = 64000;
+    private static final int FILTER_SIZE = 128000;
     private static final int HASH_COUNT = 2;
     private final BloomFilter bloomFilter = new BloomFilter(FILTER_SIZE, HASH_COUNT);
 
